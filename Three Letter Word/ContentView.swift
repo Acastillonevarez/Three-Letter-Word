72e14c6684a2ba52161f3f9d6b095dbe08e604f8
Andrew <acastillonevarez@bsd220.org>
2022-10-26 09:38:20 -0500

    "Created custom letter box"

--- Three Letter Word/ContentView.swift
+++ Three Letter Word/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var letter = ""
+    @State private var counter = ""
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -22,5 +25,18 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+struct CustomLetterBox: View {
+    let color : Color
+    let text : String
+    var body: some View {
+        ZStack {
+            color
+            Text(text)
+                .font(.system(size: 90))
+                .fontWeight(.heavy)
+        }
+        .frame(width: 120, height: 120, alignment: .center)
     }
 }
